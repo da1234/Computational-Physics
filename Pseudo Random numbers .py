a607e5cc2f9ad3e2aff1142cc13c8d15aabf48e3
da1234 <darrell.adjei@hotmail.com>
2017-09-28 09:41:29 +0100

    Update Pseudo Random numbers .py

--- Pseudo Random numbers .py
+++ Pseudo Random numbers .py
@@ -1,20 +1,12 @@
 
 
-def nplusone(a,c,m,n):
-    
-    return (a*n + c)%m
-    
-    
-    
-    
+def nplusone(a,c,m,n):    
+    return (a*n + c)%m              
+
 a = 1664525
-
 b= 2**32
-c,n = 1013904223, 0  
+c,n = 1013904223, 0        
 
-    
-        
-for i in range(10):
-    
+for i in range(10):    
         n = nplusone(a,c,b,n)
         print n
