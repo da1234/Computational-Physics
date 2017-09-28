c1142448292b68f0b0e0dd87cb5dbe122c55c9d0
da1234 <darrell.adjei@hotmail.com>
2017-09-28 09:47:50 +0100

    Update random.py

--- random.py
+++ random.py
@@ -1,14 +1,8 @@
 
 def nplusone(a=1664525,c=1013904223,m=2**32,n=0):
     return (a*n+c)%m
-
-
 n0=0
-
-for i in range(10):
-    
+for i in range(10):    
     n1 = nplusone(a=1664525,c=1013904223,m=2**32,n=n0)
-    n0=n1
-    
-     
+    n0=n1         
     print n1
